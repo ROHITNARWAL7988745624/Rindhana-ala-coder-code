b8bd0da233eb8e0ffd8765a21871e251e3f7c6ab
ROHITNARWAL7988745624 <79442284+ROHITNARWAL7988745624@users.noreply.github.com>
2021-03-05 22:27:37 +0530

    Add files via upload

---
+++ python tut/TUT2.py
@@ -0,0 +1,14 @@
+# print("Hello World")
+# var="Rohit is a good boy"
+# num=7
+# print(type(var))
+# print(type(num))
+# var="Rohit is a good boy"
+# print(var[-1])
+# int,float,complex
+# num=7
+# num1=16.676897
+# num2=-67j
+# print(type(num))
+# print(type(num1))
+# print(type(num2))
